11b2bc73224514fbb9a8262d0921f245a5d6677d
Bryan Ngo <bryanngo@berkeley.edu>
2022-06-04 01:22:51 -0700

    fix tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,6 +3,13 @@ use bf_rust::Program;
 #[test]
 fn test_hello_world() {
     let program = String::from("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
-    let mut machine = Program::new(program);
-    assert_eq!(String::from("Hello World!\n"), machine.run().unwrap());
+    let machine = Program::new(&[String::from(""), program]);
+    assert_eq!(String::from("Hello World!\n"), machine.unwrap().run().unwrap());
+}
+
+#[test]
+fn test_hello_world_2() {
+    let program = String::from(">++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+.");
+    let machine = Program::new(&[String::from(""), program]);
+    assert_eq!(String::from("Hello World!\n"), machine.unwrap().run().unwrap());
 }
