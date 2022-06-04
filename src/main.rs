06dda289d164e3609b98f659ab1962715de8bced
Bryan Ngo <bryanngo@berkeley.edu>
2022-06-04 01:22:59 -0700

    add runtime

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+use std::{env, process};
 use bf_rust::Program;
 
 fn main() {
-    println!("Hello, world!");
+    let argv: Vec<String> = env::args().collect();
+    let machine = Program::new(&argv);
+    match machine {
+        Ok(mut p) => {
+            match p.run() {
+                Ok(s) => print!("{}", s),
+                Err(e) => {
+                    eprintln!("{}", e);
+                    process::exit(1);
+                },
+            }
+        },
+        Err(e) => {
+            eprintln!("{}", e);
+            process::exit(1);
+        },
+    }
 }
